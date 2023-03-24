3d6f9402f302601d3117a24c9e8a882d6ec109e4
Chirag <99790072+Hazard-Chirag@users.noreply.github.com>
2023-03-24 18:18:51 +0530

    Create AppletGUI_BASIC.java

---
+++ extra programs/AppletGUI_BASIC.java
@@ -0,0 +1,10 @@
+import java.applet.Applet;
+import java.awt.Graphics; 
+
+public class AppletP extends Applet
+{
+  public void paint(Graphics g)
+  {
+    g.drawString("Welcome to applet",100,150);
+  }
+}
