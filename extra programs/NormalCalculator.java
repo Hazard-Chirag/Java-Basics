2809774c497277054163d44d0273a74407fb101b
Chirag <99790072+Hazard-Chirag@users.noreply.github.com>
2023-03-24 18:29:08 +0530

    Create NormalCalculator.java

---
+++ extra programs/NormalCalculator.java
@@ -0,0 +1,32 @@
+class Calculator
+{
+public static void main(String[] args)
+{
+char op;
+ int num1, num2, result;
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter an operator");
+ op = sc.next().charAt(0);
+System.out.println("Enter first number");
+ num1 = sc.nextInt();
+System.out.println("Enter second number");
+ num2 = sc.nextInt();
+switch (op)
+{
+case '+':result = num1 + num2;
+ break;
+ case '-':result = num1 - num2;
+ break;
+
+case '*':result = num1 * num2;
+ break;
+case '/':result = num1 / num2;
+ break;
+case '%':result = num1 % num2;
+ break;
+ default: System.out.println("Invalid operator!");
+ return;
+ }
+System.out.println(num1+” “ + op + " " + num2+”=”+result);
+ }
+}
